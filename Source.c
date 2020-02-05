287f38c3446c2fa6a429d8561666da591a9ee282
Krabb <keithrabbitte@gmail.com>
2020-02-05 18:09:56 +0000

    Acceleration now determined by sum of forces

--- Source.c
+++ Source.c
@@ -2,23 +2,39 @@
 #include <math.h>
 #include <windows.h>
 
-#define PI 3.14159265359
+#define PI 3.1415926535897
 #define GRAV -9.8
 #define SLEEPRATIO interval/1000
-#define sec(sec) sec * 1000 / interval
+#define sec(sec) sec * 1000.0 / interval
+#define velmag point.vel.x * point.vel.x + point.vel.y * point.vel.y
+#define accmag point.acc.x * point.acc.x + point.acc.y * point.acc.y
+#define setpointfield(a,b) point.a.x = b; point.acc.y = b;
+
 
 int updatepos();
+int checkforces();
+int clearforce();
 
 typedef struct force {
 	double x, y;
 	double magnitude;
+	double angle;
 	double radius;
 }force;
 
+typedef struct acceleration {
+	double x, y;
+}acceleration;
+
+typedef struct velocity {
+	double x, y;
+}velocity;
+
 typedef struct object {
 	double x, y;
-	double vel, acc;
 	double mass;
+	acceleration vel;
+	velocity acc;
 	force forces[20];
 }obj;
 
@@ -30,24 +46,37 @@ int main()
 {
 	int time = 0;
 
-	for (int i = 0; i < 20; i++)
-		point.forces[i].magnitude = 0;
+	interval = 1;
 
-	point.forces[0].magnitude = 2;
-	point.forces[1].magnitude = -1;
-	point.vel = 0;
-	interval = 10;
-	angle = 45;
+	force force1, force2;
 
-	while (time < sec(30))
+	force1.angle = 40;
+	force1.magnitude = 6;
+	force2.angle = 270;
+	force2.magnitude = 5;
+
+	while (time < sec(30.0))
 	{
-		/* Print coordinates every sec(x) seconds */
-		point.acc = 0;
-		for (int i = 0; i < 20; i++)
-			point.acc += point.forces[i].magnitude;
 
-		if (fmod(time, sec(0.1)) == 0)
-			printf("Time: %0.3f	Vel = %0.3f	(%0.5f , %0.5f)\n", (double)time * SLEEPRATIO, point.vel, point.x, point.y);
+		for (int i = 0; i < 20; i++)
+		{
+			clearforce();
+			setpointfield(acc, 0);
+		}
+
+		point.forces[0] = force1;
+		point.forces[1] = force2;
+
+		for (int i = 0; i < 20; i++)
+		{
+			point.acc.x += point.forces[i].magnitude * cos(point.forces[i].angle * (PI/180));
+			point.acc.y += point.forces[i].magnitude * sin(point.forces[i].angle * (PI / 180));
+		}
+
+		if (fmod(time, sec(1)) == 0)
+		{
+			printf("Time: %0.3f\tVel = %0.3f\tAcc = %0.3f\t(%0.2f , %0.2f)\n", (double)time * SLEEPRATIO, sqrt(velmag), sqrt(accmag), point.x, point.y);
+		}
 
 		Sleep(interval);
 		updatepos();
@@ -60,10 +89,30 @@ int main()
 
 int updatepos()
 {
-	point.vel += (point.acc * (SLEEPRATIO));
+	point.vel.x += (point.acc.x * (SLEEPRATIO));
+	point.vel.y += (point.acc.y * (SLEEPRATIO));
 
-	point.x += point.vel * cos(angle * (PI / 180)) * SLEEPRATIO;
-	point.y += point.vel * sin(angle * (PI / 180)) * SLEEPRATIO;
+	point.x += point.vel.x * SLEEPRATIO;
+	point.y += point.vel.y * SLEEPRATIO;
 
 	return 0;
+}
+
+int clearforce()
+{
+	for (int i = 0; i < 20; i++)
+	{
+		point.forces[i].x = 0;
+		point.forces[i].y = 0;
+		point.forces[i].magnitude = 0;
+		point.forces[i].angle = 0;
+		point.forces[i].radius = 0;
+	}
+
+	return 0;
+}
+
+int checkforces()
+{
+	
 }
